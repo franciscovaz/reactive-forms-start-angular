d65b8c751511443d9d9106b9dad4b3837c0459e7
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-17 10:41:53 +0000

    setting and patching values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,10 +29,26 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe((status) => {
       console.log(status);
     });
+
+    /* this.signupForm.setValue({
+      'userData': {
+        'username': 'Francisco',
+        'email': 'vaz@mail.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    }); */
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Frank'
+      }
+    })
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
